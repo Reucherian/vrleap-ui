156df6bac7d14545f2710ff76f9fc9e8f14d5c32
Reuben Cherian <reucherian@gmail.com>
2019-03-17 14:59:28 +0530

    Corrected the animation properties

--- src/views.js
+++ src/views.js
@@ -3,41 +3,47 @@ const views = AFRAME.registerComponent('views',{
         position :{default:'0 0 -2'},
         rotpos: {default:'rot0'},
         animation:{
-            default:"",
+            default:{
             property: 'rotation',
             to:'0 -60 0',
             startEvents :'rot0', 
-            repeat: '1'},
+            repeat: '1'}
+        },
         animation__2:{
-            default:"",
+            default:{
             property: 'rotation', 
             to:'0 -120 0',
             startEvents :'rot1', 
-            repeat: '1'},
+            repeat: '1'}
+        },
         animation__3:{
-            default:"",
+            default:{
             property: 'rotation', 
             to:'0 -180 0',
             startEvents :'rot2', 
-            repeat: '1'}, 
+            repeat: '1'}
+        }, 
         animation__4:{
-            default:"",
+            default:{
             property: 'rotation', 
             to:'0 -240 0',
             startEvents :'rot3', 
-            repeat: '1'},
+            repeat: '1'}
+        },
         animation__5:{
-            default:"",
+            default:{
             property: 'rotation', 
             to:'0 -300 0',
             startEvents :'rot4', 
-            repeat: '1'}, 
+            repeat: '1'}
+        }, 
         animation__6:{
-            default:"",
+            default:{
             property: 'rotation', 
             to:'0 0 0',
             startEvents :'rot5', 
-            repeat: '1'}, 
+            repeat: '1'}
+        } 
     },
     init:function(){
         // We are going to try and add all the a-cards here
