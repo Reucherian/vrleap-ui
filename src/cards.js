3ab3b29d88258c821ff9252c1310b56bbbc3710e
Reuben Cherian <reucherian@gmail.com>
2019-03-17 16:02:59 +0530

    Fixed cards need to update the position to make it work

--- src/cards.js
+++ src/cards.js
@@ -1,16 +1,30 @@
 const cards = AFRAME.registerComponent('cards', {
     // This is basically a copy of the gemoetry of a box and there really is no difference other than our modified default for depth height and width
         schema: {
+          color: {default:"red"},
           depth: {default: 0.01, min: 0},
           height: {default: 3, min: 0},
           width: {default: 3.75, min: 0},
           segmentsHeight: {default: 1, min: 1, max: 20, type: 'int'},
           segmentsWidth: {default: 1, min: 1, max: 20, type: 'int'},
-          segmentsDepth: {default: 1, min: 1, max: 20, type: 'int'}
+          segmentsDepth: {default: 1, min: 1, max: 20, type: 'int'},
+          position: {
+            type: 'vec3',
+            default: {
+              x: 0,
+              y: 0,
+              z: 0,
+            }
+          }
         },
-        init: function (data) {
-          this.geometry = new THREE.BoxGeometry(
-            data.width, data.height, data.depth, data.segmentsWidth, data.segmentsHeight, data.segmentsDepth);
+
+        init: function () {
+            var material = new THREE.LineBasicMaterial({
+            color: this.data.color
+            });
+            var geometry = new THREE.BoxGeometry(
+            this.data.width, this.data.height, this.data.depth, this.data.segmentsWidth, this.data.segmentsHeight, this.data.segmentsDepth);
+            this.el.setObject3D('card',new THREE.Mesh(geometry,material))
         }
     });
 export {cards}
