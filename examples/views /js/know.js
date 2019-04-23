bb12f8032e04ac1e9f5a576da7eda9ce47d5cb58
Roshni Venkatesh <vroshni@hotmail.com>
2019-04-23 22:31:27 +0530

    Integrated knowledge portal into final app

--- examples/views /js/know.js
+++ examples/views /js/know.js
@@ -2,4 +2,12 @@ function know(){
     // TODO : @roshni
     app = "know";
     console.log("know called");
+    var body = document.getElementById('p');
+    while (body.firstChild) {
+        body.removeChild(body.firstChild);
+    }
+    var keyboard = document.createElement('a-entity');
+    keyboard.setAttribute('keyboard','');
+    body.appendChild(keyboard);
+
 }
