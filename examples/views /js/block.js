981ab343703e93918430b1233c64ba7e58e7dc3c
Pratik Sampat <pratik.r.sampat@gmail.com>
2019-04-23 20:31:02 +0530

    Blocky app done

--- examples/views /js/block.js
+++ examples/views /js/block.js
@@ -1,4 +1,42 @@
 function block(){
-    // TODO : @pratik
+    // TODO : @pratik - Done :)
+    app = "block";
     console.log("block called");
+    var body = document.getElementById('p');
+    while (body.firstChild) {
+        body.removeChild(body.firstChild);
+    }
+    var box1 = document.createElement('a-box');
+    box1.setAttribute('width','0.25');
+    box1.setAttribute('height','0.25');
+    box1.setAttribute('depth','0.25');
+    box1.setAttribute('position','-0.125 1.73 -0.5');
+    box1.setAttribute('color','red');
+    box1.setAttribute('holdable','');
+    box1.setAttribute('dynamic-body','shape: box');
+    body.appendChild(box1);
+    var box1 = document.createElement('a-box');
+    box1.setAttribute('width','0.25');
+    box1.setAttribute('height','0.25');
+    box1.setAttribute('depth','0.25');
+    box1.setAttribute('position','0.00 2.0 -0.5');
+    box1.setAttribute('color','green');
+    box1.setAttribute('holdable','');
+    box1.setAttribute('dynamic-body','shape: box');
+    body.appendChild(box1);
+    var box1 = document.createElement('a-box');
+    box1.setAttribute('width','0.25');
+    box1.setAttribute('height','0.25');
+    box1.setAttribute('depth','0.25');
+    box1.setAttribute('position','0.125 1.73 -0.5');
+    box1.setAttribute('color','blue');
+    box1.setAttribute('holdable','');
+    box1.setAttribute('dynamic-body','shape: box');
+    body.appendChild(box1);
+
+    var grid = document.createElement('a-grid');
+    grid.setAttribute('src',"");
+    grid.setAttribute('static-body',"");
+    grid.setAttribute('position','0 1.4 0');
+    body.appendChild(grid);
 }
--- examples/views /js/glistener.js
+++ examples/views /js/glistener.js
@@ -14,10 +14,10 @@ AFRAME.registerComponent('tappable',{
                 if(self.el.id && typeof window[self.el.id] === "function"){
                     eval(self.el.id)(); 
                     // Functions to be implemented
-                    // 1. paint
-                    // 2. block
+                    // 1. paint 
+                    // 2. block ✔
                     // 3. know
-                    // 4. docs
+                    // 4. docs  ✔
                 }
             }
             else if(app == "docs"){
@@ -111,5 +111,53 @@ AFRAME.registerComponent('fistable', {
                 this.close = false;
             }
         }
+        else if(app == "block"){
+            console.log("in block");
+            var boxes = document.querySelectorAll('[holdable]');
+            for(var i=0; i<boxes.length; i++){
+              if(boxes[i].body.world.gravity.y == 0){
+                boxes[i].body.world.gravity.set(0,-9.8,0)
+              }
+              else{
+                boxes[i].body.applyImpulse(
+                /* impulse */        new CANNON.Vec3(0, 0.1, 0),
+                /* world position */ boxes[i].body.position
+                );
+                console.log(boxes[i].body.world.gravity.set(0,0,0));
+              }
+            }
+            this.handID = e.detail.handID;
+        }
     }
 });
+
+AFRAME.registerComponent('holdable', {
+    schema: {activeColor: {default: 'orange'}},
+
+    init: function () {
+      this.physics =    /** @type {AFRAME.System}     */ this.el.sceneEl.systems.physics;
+      this.constraint = /** @type {CANNON.Constraint} */ null;
+      this.handID =     /** @type {number} */            null;
+      this.el.addEventListener('leap-holdstart', this.onHoldStart.bind(this));
+      this.el.addEventListener('leap-holdstop', this.onHoldStop.bind(this));
+    },
+
+    onHoldStart: function (e) {
+      if (this.handID) return;
+
+      this.originalColor = this.el.getAttribute('material').color;
+      this.el.setAttribute('material', 'color', this.data.activeColor);
+      this.constraint = new CANNON.LockConstraint(this.el.body, e.detail.body);
+      this.physics.addConstraint(this.constraint);
+      this.handID = e.detail.handID;
+    },
+
+    onHoldStop: function (e) {
+      if (e.detail.handID !== this.handID) return;
+
+      this.el.setAttribute('material', 'color', this.originalColor);
+      this.physics.removeConstraint(this.constraint);
+      this.constraint = null;
+      this.handID = null;
+    }
+});   
